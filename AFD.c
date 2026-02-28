8ef824db578fda92746e4f6d196454f9744633d7
roxo-1 <carolzinhalee09@gmail.com>
2026-02-28 12:18:45 -0300

    COMMIT função lerarquivo e gitignore

--- AFD.c
+++ AFD.c
@@ -1,6 +1,44 @@
+// Bibliotecas
 #include <stdio.h>
-#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Definindo limites
+#define MAX_LINHAS 100
+#define TAM_LINHAS 256
+
+//Para armazenar as linhas
+typedef struct{
+    char texto[MAX_LINHAS][TAM_LINHAS]; //matriz
+    int qtd; //qtd de linhas
+}ListaDeLinhas;
+
+//Função que lê o arquivo, ignora as linhas com '#' e armazena as outras no struct
+void lerArquivo(const char *Nomearquivo, ListaDeLinhas *lista){
+    FILE *arquivo = fopen(Nomearquivo, "r");
+    lista->qtd =0;
+    if(arquivo==NULL){
+        printf("Erro: Não foi possível abrir o arquivo!");
+        return;
+    }
+    char linha[TAM_LINHAS];
+    while(fgets(linha, TAM_LINHAS, arquivo) != NULL){
+        if(linha[0]=='#'){
+            continue; //força o loop a continuar, ignorando essa linha
+        }
+        if(lista->qtd >= MAX_LINHAS){ // Se a lista encher, para de ler
+            printf("Limite de linhas atingido!");
+            break;
+        }
+        // Copia a linha para a lista de listas, onde armazenaremos as linhas para usá-las depois
+        strcpy(lista->texto[lista->qtd], linha);//destino -> origem, ou seja, veio do buffer "linha" e vai para a lista de lista
+        lista->qtd++;
+    }
+    fclose(arquivo);
+}
 
 int main(){
+    ListaDeLinhas entrada;
+    lerArquivo("entradaAFD.txt", &entrada);
     return 0;
 }
